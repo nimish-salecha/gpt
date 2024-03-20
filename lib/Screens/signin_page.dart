0415c35d7519ae91d47b5570726efef4142fcf9a
nimish <developingdeveloper0@gmail.com>
2024-03-21 03:11:23 +0530

    front end 1

--- lib/Screens/signin_page.dart
+++ lib/Screens/signin_page.dart
@@ -4,182 +4,184 @@ import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:gpt/Screens/home.dart';
 
-// class SignInPage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Color(0xFF9DBC98),
-//       body: Center(
-//         child: Padding(
-//           padding: const EdgeInsets.all(16.0),
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: [
-// // Logo
-//               Image.asset(
-//                 'assets/logo.png',
-//                 height: 80,
-//                 width: 80,
-//               ),
-//               Container(
-//                 padding: EdgeInsets.all(25),
-//                 margin: EdgeInsets.only(top: 10),
-//                 decoration: BoxDecoration(
-//                   borderRadius: BorderRadius.circular(20.0),
-//                   border: Border.all(
-//                     color: Color.fromARGB(
-//                         255, 42, 42, 42), // Set your desired underline color
-//                     width: 2.0, // Set the underline thickness
-//                   ),
-//                 ),
-//                 child: Column(
-//                   children: [
-//                     SizedBox(height: 16),
+class SignInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF9DBC98),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+// Logo
+              Image.asset(
+                'assets/logo.png',
+                height: 80,
+                width: 80,
+              ),
+              Container(
+                padding: EdgeInsets.all(25),
+                margin: EdgeInsets.only(top: 10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.0),
+                  border: Border.all(
+                    color: Color.fromARGB(
+                        255, 42, 42, 42), // Set your desired underline color
+                    width: 2.0, // Set the underline thickness
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    SizedBox(height: 16),
 
-//                     Text(
-//                       'Sign In',
-//                       style: TextStyle(
-//                         fontSize: 24,
-//                         fontWeight: FontWeight.bold,
-//                         color: Colors.white,
-//                       ),
-//                     ),
-//                     SizedBox(height: 16),
-//                     // Email TextField
-//                     SizedBox(
-//                       width: 300,
-//                       child: TextField(
-//                         decoration: InputDecoration(
-//                           hintText: 'Email',
-//                           fillColor: Colors.white,
-//                           filled: true,
-//                           border: OutlineInputBorder(
-//                             borderRadius: BorderRadius.circular(10),
-//                             borderSide: BorderSide.none,
-//                           ),
-//                           contentPadding: EdgeInsets.symmetric(
-//                               horizontal: 16, vertical: 12),
-//                         ),
-//                       ),
-//                     ),
-//                     SizedBox(height: 16),
-//                     // Password TextField
-//                     SizedBox(
-//                       width: 300,
-//                       child: TextField(
-//                         obscureText: true,
-//                         decoration: InputDecoration(
-//                           hintText: 'Password',
-//                           fillColor: Colors.white,
-//                           filled: true,
-//                           border: OutlineInputBorder(
-//                             borderRadius: BorderRadius.circular(10),
-//                             borderSide: BorderSide.none,
-//                           ),
-//                           contentPadding: EdgeInsets.symmetric(
-//                               horizontal: 16, vertical: 12),
-//                         ),
-//                       ),
-//                     ),
-//                     SizedBox(height: 16),
-// // Forget Password Link
-//                     Container(
-//                       // padding: EdgeInsets.symmetric(vertical: 10.0),
-//                       decoration: BoxDecoration(
-//                         border: Border(
-//                           bottom: BorderSide(
-//                             color:
-//                                 Colors.blue, // Set your desired underline color
-//                             width: 2.0, // Set the underline thickness
-//                           ),
-//                         ),
-//                       ),
-//                       child: GestureDetector(
-//                         onTap: () {
-//                           // Implement forget password logic here
-//                         },
-//                         child: Text(
-//                           'Forget Password?',
-//                           style: TextStyle(
-//                             color: Colors.white,
-//                           ),
-//                         ),
-//                       ),
-//                     ),
-//                     SizedBox(height: 16),
-//                     // Sign In Button
-//                     ElevatedButton(
-//                       onPressed: () {
-//                         // Implement sign-in logic here
-//                       },
-//                       child: Text('Sign In'),
-//                       style: ElevatedButton.styleFrom(
-//                         backgroundColor: Colors.blue,
-//                         shape: RoundedRectangleBorder(
-//                           borderRadius: BorderRadius.circular(10),
-//                         ),
-//                         padding:
-//                             EdgeInsets.symmetric(horizontal: 32, vertical: 12),
-//                       ),
-//                     ),
-//                     SizedBox(height: 16),
-//                     // Sign In with Google Button
-//                     OutlinedButton(
-//                       onPressed: () {
-//                         // Implement sign-in with Google logic here
-//                       },
-//                       style: OutlinedButton.styleFrom(
-//                         foregroundColor: Colors.white,
-//                         shape: RoundedRectangleBorder(
-//                           borderRadius: BorderRadius.circular(10),
-//                         ),
-//                         side: BorderSide(color: Colors.white),
-//                         padding:
-//                             EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-//                       ),
-//                       child: Row(
-//                         mainAxisSize: MainAxisSize.min,
-//                         children: [
-//                           Image.asset('assets/google_logo.png',
-//                               height: 20, width: 20),
-//                           SizedBox(width: 8),
-//                           Text('Sign In with Google'),
-//                         ],
-//                       ),
-//                     ),
-//                   ],
-//                 ),
-//               ), //border of feilds
-//               SizedBox(height: 16),
-//               Container(
-//                 child: Row(mainAxisSize: MainAxisSize.min, children: [
-//                   Text(
-//                     "Don't have account?  ",
-//                     style: TextStyle(
-//                       color: Colors.white,
-//                     ),
-//                   ),
-//                   GestureDetector(
-//                     onTap: () {
-//                       Navigator.pushNamed(context, '/signup');
-//                     },
-//                     child: Text(
-//                       "Create One",
-//                       style: TextStyle(
-//                           color: Colors.blue,
-//                           fontSize: 20,
-//                           fontWeight: FontWeight.w700),
-//                     ),
-//                   ),
-//                 ]),
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
+                    Text(
+                      'Sign In',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    // Email TextField
+                    SizedBox(
+                      width: 300,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Email',
+                          fillColor: Colors.white,
+                          filled: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide.none,
+                          ),
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 16, vertical: 12),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    // Password TextField
+                    SizedBox(
+                      width: 300,
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          fillColor: Colors.white,
+                          filled: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide.none,
+                          ),
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 16, vertical: 12),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 16),
+// Forget Password Link
+                    Container(
+                      // padding: EdgeInsets.symmetric(vertical: 10.0),
+                      decoration: BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
+                            color:
+                                Colors.blue, // Set your desired underline color
+                            width: 2.0, // Set the underline thickness
+                          ),
+                        ),
+                      ),
+                      child: GestureDetector(
+                        onTap: () {
+                          // Implement forget password logic here
+                        },
+                        child: Text(
+                          'Forget Password?',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    // Sign In Button
+                    ElevatedButton(
+                      onPressed: () {
+                        // Implement sign-in logic here
+                      },
+                      child: Text('Sign In'),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 32, vertical: 12),
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    // Sign In with Google Button
+                    OutlinedButton(
+                      onPressed: () {
+                        // Implement sign-in with Google logic here
+                      },
+                      style: OutlinedButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        side: BorderSide(color: Colors.white),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                      ),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Image.asset('assets/google_logo.png',
+                              height: 20, width: 20),
+                          SizedBox(width: 8),
+                          Text('Sign In with Google'),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ), //border of feilds
+              SizedBox(height: 16),
+              Container(
+                child: Row(mainAxisSize: MainAxisSize.min, children: [
+                  Text(
+                    "Don't have account?  ",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(context, '/signup');
+                    },
+                    child: Text(
+                      "Create One",
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700),
+                    ),
+                  ),
+                ]),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/*
 final GoogleSignIn _googleSignIn = GoogleSignIn();
 
 class SignInPage extends StatelessWidget {
@@ -440,6 +442,7 @@ void _signInWithGoogle(BuildContext context) async {
   }
 }
 
+*/
 
 // import 'package:flutter/material.dart';
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,145 @@
-// ignore_for_file: prefer_const_constructors, unused_import
+//yt 
 
+// ignore_for_file: sort_child_properties_last
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gpt/Services/AuthenticationService.dart';
+import 'package:gpt/screens/RegistrationScreen.dart';
+import 'package:gpt/screens/home.dart';
+
+void main() => runApp(MaterialApp(
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => LoginScreen(),
+        '/register': (context) => RegistrationScreen(),
+        '/dashboard': (context) => Home(),
+      },
+    ));
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _key = GlobalKey<FormState>();
+
+  final AuthenticationService _auth = AuthenticationService();
+
+  TextEditingController _emailContoller = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.deepPurple,
+        child: Center(
+          child: Form(
+            key: _key,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Login',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(32.0),
+                  child: Column(
+                    children: [
+                      SizedBox(height: 30),
+                      TextFormField(
+                        controller: _emailContoller,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Email cannot be empty';
+                          } else
+                            return null;
+                        },
+                        decoration: InputDecoration(
+                            labelText: 'Email',
+                            labelStyle: TextStyle(color: Colors.white)),
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      SizedBox(height: 30),
+                      TextFormField(
+                        controller: _passwordController,
+                        obscureText: true,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Password cannot be empty';
+                          } else
+                            return null;
+                        },
+                        decoration: InputDecoration(
+                            labelText: 'Password',
+                            labelStyle: TextStyle(color: Colors.white)),
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      TextButton(
+                        child: Text('Not registerd? Sign up',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).push(
+                            CupertinoPageRoute(
+                              fullscreenDialog: true,
+                              builder: (context) => RegistrationScreen(),
+                            ),
+                          );
+                        },
+                      ),
+                      SizedBox(height: 30),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          TextButton(
+                            child: Text('Login'),
+                            onPressed: () {
+                              if (_key.currentState!.validate()) {
+                                signInUser();
+                              }
+                            },
+                            style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.white)),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void signInUser() async {
+    dynamic authResult =
+        await _auth.loginUser(_emailContoller.text, _passwordController.text);
+    if (authResult == null) {
+      print('Sign in error. could not be able to login');
+    } else {
+      _emailContoller.clear();
+      _passwordController.clear();
+      Navigator.pushNamed(context, '/dashboard');
+    }
+  }
+}
+
+
+// ignore_for_file: prefer_const_constructors, unused_import
+/*
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart';
@@ -13,16 +153,16 @@ import 'package:gpt/screens/signup_page.dart';
 import 'screens/header.dart';
 // import 'screens/home.dart';
 
-// void main() => runApp(MyApp());
+void main() => runApp(MyApp());
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  // addUser("sher", "s@gmail.com");
-  runApp(MyApp());
-}
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+//   );
+//   // addUser("sher", "s@gmail.com");
+//   runApp(MyApp());
+// }
 
 // ignore: use_key_in_widget_constructors
 class MyApp extends StatelessWidget {
@@ -42,8 +182,10 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+*/
 
 //-------firebase cde- hiyu------
+/*
 Future<void> addUser(String name, String email) {
   CollectionReference users = FirebaseFirestore.instance.collection('users');
 
@@ -109,48 +251,4 @@ Future<void> fetchUsersAboveAge(int minAge) {
     });
   }).catchError((error) => print("Failed to fetch users: $error"));
 }
-//-----------------
-
-// // ignore: use_key_in_widget_constructors
-// class HomePage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       // appBar: AppBar(
-//       //   title: Text('GPT App'),
-//       //   actions: [
-//       //     TextButton(
-//       //       onPressed: () {
-//       //         Navigator.pushNamed(context, '/signin');
-//       //       },
-//       //       child: Text(
-//       //         'Sign In',
-//       //         style: TextStyle(color: Colors.black),
-//       //       ),
-//       //     ),
-//       //     TextButton(
-//       //       onPressed: () {
-//       //         Navigator.pushNamed(context, '/signup');
-//       //       },
-//       //       child: Text(
-//       //         'Sign Up',
-//       //         style: TextStyle(color: Colors.black),
-//       //       ),
-//       //     )
-//       //   ],
-//       // ),
-//       body: Column(
-//         children: [
-//           Header(),
-//           // Home(),
-//           // Your page content goes here
-//            Text('Welcome to my app!'),
-//           // Footer(),
-//         ],
-//       ),
-//       // Center(
-//       //   child: Text('Welcome to GPT App!'),
-//       // ),
-//     );
-//   }
-// }
+*/
