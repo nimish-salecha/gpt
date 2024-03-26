175bdacd8f7345f6ed81f92d50fbfbafeae572fa
nimish <developingdeveloper0@gmail.com>
2024-03-26 18:13:50 +0530

    signin/up frontend

--- lib/Screens/login_register_page.dart
+++ lib/Screens/login_register_page.dart
@@ -1,4 +1,178 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:gpt/Services/AuthenticationService.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String? errorMessage = '';
+  bool isLogin = true;
+
+  final TextEditingController _controllerEmail = TextEditingController();
+  final TextEditingController _controllerPassword = TextEditingController();
+
+  Future<void> signInWithEmailAndPassword() async {
+    try {
+      await Auth().signInWithEmailAndPassword(
+        email: _controllerEmail.text,
+        password: _controllerPassword.text,
+      );
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        errorMessage = e.message;
+      });
+    }
+  }
+
+  Future<void> createUserWithEmailAndPassword() async {
+    try {
+      await Auth().createUserWithEmailAndPassword(
+        email: _controllerEmail.text,
+        password: _controllerPassword.text,
+      );
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        errorMessage = e.message;
+      });
+    }
+  }
+
+  Widget _header(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          isLogin ? 'Welcome Back' : 'Sign up',
+          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+        ),
+        Text(isLogin ? 'Enter your credentials to login' : 'Create your account'),
+      ],
+    );
+  }
+  Widget _inputField(BuildContext context) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.stretch,
+    children: [
+      TextField(
+        controller: _controllerEmail,
+        decoration: InputDecoration(
+          hintText: 'Email',
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(18),
+            borderSide: BorderSide.none,
+          ),
+          fillColor: Colors.purple.withOpacity(0.1),
+          filled: true,
+          prefixIcon: Icon(Icons.email),
+        ),
+      ),
+      SizedBox(height: 10),
+      // if (!isLogin) ...[  //only front end
+      //   TextField(
+      //     decoration: InputDecoration(
+      //       hintText: 'Username',
+      //       border: OutlineInputBorder(
+      //         borderRadius: BorderRadius.circular(18),
+      //         borderSide: BorderSide.none,
+      //       ),
+      //       fillColor: Colors.purple.withOpacity(0.1),
+      //       filled: true,
+      //       prefixIcon: Icon(Icons.person),
+      //     ),
+      //   ),
+      //   SizedBox(height: 10),
+      // ],
+        TextField(
+        controller: _controllerPassword,
+        decoration: InputDecoration(
+          hintText: 'Password',
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(18),
+            borderSide: BorderSide.none,
+          ),
+          fillColor: Colors.purple.withOpacity(0.1),
+          filled: true,
+          prefixIcon: Icon(Icons.password),
+        ),
+        obscureText: true,
+      ),
+      SizedBox(height: 10),
+      // if (!isLogin) ...[      //only front end
+      //   TextField(
+      //     decoration: InputDecoration(
+      //       hintText: 'Confirm Password',
+      //       border: OutlineInputBorder(
+      //         borderRadius: BorderRadius.circular(18),
+      //         borderSide: BorderSide.none,
+      //       ),
+      //       fillColor: Colors.purple.withOpacity(0.1),
+      //       filled: true,
+      //       prefixIcon: Icon(Icons.password),
+      //     ),
+      //     obscureText: true,
+      //   ),
+      //   SizedBox(height: 10),
+      // ],
+      
+      ElevatedButton(
+        onPressed: isLogin ? signInWithEmailAndPassword : createUserWithEmailAndPassword,
+        style: ElevatedButton.styleFrom(
+          shape: StadiumBorder(),
+          padding: EdgeInsets.symmetric(vertical: 16),
+          backgroundColor: Colors.purple,
+        ),
+        child: Text(isLogin ? 'Login' : 'Sign up', style: TextStyle(fontSize: 20)),
+      ),
+    ],
+  );
+}
+
+  Widget _errorMessage() {
+    return Text(errorMessage == '' ? '' : 'Hmm? $errorMessage');
+  }
+
+  Widget _loginOrRegisterButton() {
+    return TextButton(
+      onPressed: () {
+        setState(() {
+          isLogin = !isLogin;
+        });
+      },
+      child: Text(isLogin ? 'Register instead' : 'Login instead'),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('NexoDeb8')),    //can add app logo, applogo
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _header(context),
+              _inputField(context),
+              _errorMessage(),
+              _loginOrRegisterButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+//original main -- working -- without front end design
+/*
+import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart'; // your proj name in firebase
 import 'package:gpt/Services/AuthenticationService.dart';
 
@@ -104,3 +278,4 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 }
+*/
